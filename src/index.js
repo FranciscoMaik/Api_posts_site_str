c99fabe42819cce7935e29b2b632dfb4f8f3f487
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-26 00:46:06 -0300

    adiçao do cors e variavel de ambiente

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 
+app.use(cors());
 app.use(express.json()); ///para receber e enviar arquivos json
 app.use(routes); //usando as rotas e tem que ficar abaixo do json; top-down;
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
