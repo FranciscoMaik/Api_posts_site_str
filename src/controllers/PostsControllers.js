09f7d5af10d04dec35a1e672f88c399851af9219
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-28 22:13:40 -0300

    altenticaçao para deletar produto

--- src/controllers/PostsControllers.js
+++ src/controllers/PostsControllers.js
@@ -29,8 +29,6 @@ module.exports = {
       idadmin,
     } = request.body;
 
-    console.log(idadmin);
-
     if (validated.isUUID(idadmin)) {
       const administrador = await database("admin")
         .where("id", "=", idadmin)
@@ -59,8 +57,26 @@ module.exports = {
   },
 
   async delete(request, response) {
+    const idadmin = request.headers.authorization;
     const { id } = request.params;
-    await database("posts").where({ id }).del();
+
+    if (validated.isUUID(idadmin)) {
+      const administrador = await database("admin")
+        .where("id", "=", idadmin)
+        .select("*")
+        .first();
+
+      if (administrador) {
+        await database("posts").where({ id }).del();
+
+        return response.status(200).send();
+      } else {
+        return response.status(401).send();
+      }
+    } else {
+      return response.status(401).send();
+    }
+
     return response.status(200).send();
   },
 };
